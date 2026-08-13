9b694a5338aa9629c44eb59d0d2c0af2f79c57dd
tukaalesh <tukaalesh8@gmail.com>
2026-08-13 16:31:09 +0300

    final color

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF771011);
+  static const Color primary = Color(0XFF465973);
   static const Color primaryLight = Color(0xFF6F635B);
   static const Color primaryDark = Color(0xFF8B3214);
   static const Color secondary = Color(0xFFF5A623);
   static const Color secondaryLight = Color(0xFFFFC84A);
-  static const Color accent = Color(0xFF561C24);
-  static const Color accentLight = Color(0xFF6D2932);
+  static const Color accent = Color(0XFF465973);
+  static const Color accentLight = Color(0xff7c8ea6);
 
   static const Color background = Color(0xFFFAF7F2);
   static const Color surface = Color(0xFFFFFFFF);
